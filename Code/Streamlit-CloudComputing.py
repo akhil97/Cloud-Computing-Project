775458603dc4fefecf730ff2e67cab6806fd7a7f
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2023-11-27 22:11:29 -0500

    Update Streamlit-CloudComputing.py

--- Code/Streamlit-CloudComputing.py
+++ Code/Streamlit-CloudComputing.py
@@ -3,3 +3,20 @@ import streamlit as st
 st.set_page_config(page_title="Team 3 Cloud Computing Project", page_icon=":tada:", layout="wide")
 
 st.subheader("This is project to predict selling price of used cars")
+
+
+
+
+
+odometer_value = st.number_input("Odometer Value", min_value=0)
+engine_type = st.selectbox("Engine Type", ['Gas', 'Diesel', 'Electric'])
+body_type = st.selectbox("Body Type", ['Sedan', 'SUV', 'Truck'])
+drivetrain = st.selectbox("Drivetrain", ['AWD', 'FWD', 'RWD'])
+production_year = st.number_input("Production Year", min_value=1900, max_value=2023, step=1)
+manufacturer_name = st.selectbox("Manufacturer Name", ['Toyota', 'Ford', 'Tesla'])
+
+if st.button("Predict Price"):
+    input_data = [odometer_value, engine_type, body_type, drivetrain, production_year, manufacturer_name]
+    
+    predicted_price = predict_price(None, input_data)
+    st.success(f"Predicted Price: ${predicted_price:,.2f}")
