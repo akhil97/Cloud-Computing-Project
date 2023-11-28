19cd110a58fab3e6b108c8033a41a722f847d1f5
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2023-11-27 21:39:33 -0500

    Update Streamlit-CloudComputing.py

---
+++ Code/Streamlit-CloudComputing.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Team 3 Cloud Computing Project", page_icon=":tada:", layout="wide")
+
+st.subheader("This is project to predict selling price of used cars")
