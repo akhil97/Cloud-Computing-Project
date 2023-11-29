1c0a2180cd02b560d5b84f73540ce8a7cb18293b
akhil97 <akhilm97@gmail.com>
2023-11-28 22:39:00 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Code/Streamlit-CloudComputing.py

--- Code/Streamlit-CloudComputing.py
+++ Code/Streamlit-CloudComputing.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from sagemaker import image_uris, session
 from sagemaker.model import Model
 from sagemaker.pipeline import PipelineModel
@@ -8,9 +10,15 @@ st.set_page_config(page_title="Team 3 Cloud Computing Project", page_icon=":tada
 
 st.subheader("This is project to predict selling price of used cars")
 
+le = LabelEncoder()
+
 #Inputs from user
 odometer_value = st.number_input("Odometer Value", min_value=0)
 engine_type = st.selectbox("Engine Type", ['Gas', 'Diesel', 'Electric'])
+engine_fuel_type = st.selectbox("Engine Fuel Type", ['Diesel', 'Electric', 'Gas', 'Gasoline', 'Hybrid-Diesel', 'Hybrid-Petrol'])
+color = st.selectbox("Color", ['silver' 'blue' 'red' 'black' 'grey' 'other' 'brown' 'white' 'green'
+                               'violet' 'orange' 'yellow'])
+transmission_type = st.selectbox("Transmission Type", ['Automatic', 'Manual'])
 body_type = st.selectbox("Body Type", ['Sedan', 'SUV', 'Truck'])
 drivetrain = st.selectbox("Drivetrain", ['AWD', 'FWD', 'RWD'])
 production_year = st.number_input("Production Year", min_value=1900, max_value=2023, step=1)
@@ -18,29 +26,35 @@ manufacturer_name = st.selectbox("Manufacturer Name", ['Toyota', 'Ford', 'Tesla'
 
 #Default values
 model_name = 300
-color = 0
-transmission_automatic = 1
-transmission_mechanical = 0
+# color =
+transmission_automatic = 1 if transmission_type == 'Automatic' else 0
+transmission_mechanical = 0 if transmission_type == 'Manual' else 0
 engine_type_diesel = 1 if engine_type == 'Diesel' else 0
 engine_type_electric = 1 if engine_type == 'Electric' else 0
 engine_type_gas = 1 if engine_type == 'Gas' else 0
-engine_fuel_diesel = engine_type_diesel
-engine_fuel_electric = engine_type_electric
-engine_fuel_gas = engine_type_gas
-engine_fuel_gasoline = 0
-engine_fuel_hybrid_diesel = 0
-engine_fuel_hybrid_petrol = 0
+engine_fuel_diesel = 1 if engine_fuel_type == 'Diesel' else 0
+engine_fuel_electric = 1 if engine_fuel_type == 'Electric' else 0
+engine_fuel_gas = 1 if engine_fuel_type == 'Gas' else 0
+engine_fuel_gasoline = 1 if engine_fuel_type == 'Gasoline' else 0
+engine_fuel_hybrid_diesel = 1 if engine_fuel_type == 'Hybrid-Diesel' else 0
+engine_fuel_hybrid_petrol = 1 if engine_fuel_type == 'Hybrid-Petrol' else 0
 
 
 def predict_price(input_data):
     input_df = pd.DataFrame(input_data)
+    input_df['color'] = dict(zip(le.classes_, le.transform(le.classes_))).values()
+    input_df['body_type'] = dict(zip(le.classes_, le.transform(le.classes_))).values()
+    input_df['drivetrain'] = dict(zip(le.classes_, le.transform(le.classes_))).values()
+
+    input_array = np.array(input_df)
+
     xgb_image = image_uris.retrieve("xgboost", session.Session().boto_region_name, repo_version="latest")
     xgb_model = Model(model_data="s3://myccprojectbucket/output/sagemaker-xgboost-2023-11-29-01-52-54-334/output//model.tar.gz", image_uri=xgb_image)
 
     sagemaker_model = "sagemaker-xgboost-2023-11-29-01-56-43-900"
 
     sm_model = PipelineModel(name=sagemaker_model, role=data_scientist, models=[xgb_model])
-    price = sm_model.predict(input_df).decode('utf-8')
+    price = sm_model.predict(input_array).decode('utf-8')
     return price
 
 
@@ -52,7 +66,7 @@ if st.button("Predict Price"):
                   engine_fuel_hybrid_diesel, engine_fuel_hybrid_petrol, engine_type_diesel, engine_type_electric,
                   engine_fuel_gasoline, body_type, drivetrain]
     
-    predicted_price = predict_price(None, input_data)
+    predicted_price = predict_price(input_data)
     st.success(f"Predicted Price: ${predicted_price:,.2f}")
 
 
